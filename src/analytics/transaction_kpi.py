9e274666c3c680b86dde9db93978383f15402d50
Aishwarya Amar Patankar <106923039+aishwaryap95@users.noreply.github.com>
2026-04-28 07:19:07 +0530

    5th transaction kpi completed

--- src/analytics/transaction_kpi.py
+++ src/analytics/transaction_kpi.py
@@ -2,15 +2,23 @@ from pyspark.sql.types import *
 from pyspark.sql.functions import *
 from src.utils.logging_config import logger
 
+# daily transaction volume
+# success rate
+# revenue by channel
+# city-wise payment amount
+# monthly success rate by channel
+
 def run_transaction_kpis(df):
-    logger.info("KPI 1 : Daily Transaction Volume")
+
+    logger.info("=============== KPI 1 : Daily Transaction Volume ===============")
 
     df.groupBy(date_format(col("transaction_timestamp"), "yyyy-MM-dd").alias("Day")) \
         .agg(count("transaction_id").alias("daily_transaction_volume")) \
         .orderBy("Day") \
         .show(truncate=False)
 
-    logger.info("KPI 2 : Success Rate")
+
+    logger.info("=============== KPI 2 : Success Rate ===============")
 
     df.agg((sum(
         when(col("transaction_status") == "SUCCESS", 1)
@@ -18,7 +26,8 @@ def run_transaction_kpis(df):
     ) * 100.0 / count("*")).alias("success_rate")) \
         .show()
 
-    logger.info("KPI 3 : Revenue by channel")
+
+    logger.info("=============== KPI 3 : Revenue by channel ===============")
 
     df.filter(col("transaction_status") == "SUCCESS") \
         .groupBy("channel_type") \
@@ -26,7 +35,8 @@ def run_transaction_kpis(df):
         .orderBy(col("channel_revenue").desc()) \
         .show(truncate=False)
 
-    logger.info("KPI 4 : city-wise payment amount")
+
+    logger.info("=============== KPI 4 : city-wise payment amount ===============")
 
     df.filter(col("transaction_status") == "SUCCESS") \
         .groupBy("city").agg(
@@ -39,20 +49,21 @@ def run_transaction_kpis(df):
 
     # Ticket size is the average value of each transaction. It helps compare payment behavior across cities, merchants, or channels.
 
-    logger.info("KPI 5 : monthly success rate by channel")
 
-# +------------+---------------+
-# |channel_type|channel_revenue|
-# +------------+---------------+
-# |UPI         |21060.45       |
-# |CARD        |18572.14       |
-# |WALLET      |7871.3         |
-# |QR          |6797.11        |
-# |NET_BANKING |5972.42        |
-# +------------+---------------+
-
-# daily transaction volume
-# success rate
-# revenue by channel
-# city-wise payment amount
-# monthly success rate by channel
+    logger.info("=============== KPI 5 : monthly success rate by channel ===============")
+    df.groupBy(
+        date_format(col("transaction_timestamp"), "yyyy-MM").alias("month"),
+        col("channel_type")
+    ) \
+        .agg(
+        round(
+            (
+                sum(
+                    when(col("transaction_status") == "SUCCESS", 1)
+                    .otherwise(0)
+                ) * 100.0 / count("*")
+            ),
+            2
+        ).alias("success_rate")
+    ).orderBy("month", "channel_type") \
+        .show(truncate=False)
